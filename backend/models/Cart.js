c42e66d96ee29dd7d876c825385f4e95f46a99d1
shashika4110 <shalika4110@gmail.com>
2026-04-25 23:57:13 +0530

    Cart.js

---
+++ backend/models/Cart.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const cartItemSchema = new mongoose.Schema(
+  {
+    foodName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: 1,
+      default: 1,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+  },
+  { _id: true }
+);
+
+const cartSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+    items: [cartItemSchema],
+    subtotal: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Cart", cartSchema);
