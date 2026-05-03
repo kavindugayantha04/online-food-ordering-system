cf205674b3fb0d94b6a7669e4845f8b8dc3dc207
shashika4110 <shalika4110@gmail.com>
2026-05-03 07:20:57 +0530

    Update Voucher

--- backend/routes/voucherRoutes.js
+++ backend/routes/voucherRoutes.js
@@ -1,14 +1,4 @@
 const express = require("express");
-const authMiddleware = require("../middleware/authMiddleware");
-const uploadVoucherImage = require("../middleware/uploadVoucherMiddleware");
-const { applyVoucher } = require("../controllers/voucherController");
-
-const router = express.Router();
-
-router.post("/apply", authMiddleware, uploadVoucherImage.single("voucherImage"), applyVoucher);
-
-module.exports = router;
-const express = require("express");
 const multer = require("multer");
 const path = require("path");
 const { applyVoucher } = require("../controllers/voucherController");
