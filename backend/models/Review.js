2cedc131b6ea40599f566ff699d8314bc8e9d28b
Rayon Senuka <rayonfernando104@gmail.com>
2026-05-03 09:25:02 +0530

    Add files via upload

---
+++ backend/models/Review.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+    comment: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    photo: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Review", reviewSchema);
